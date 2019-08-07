50829b93a590349e627263b58df675f95239026e
thule <lunthu@gmail.com>
2019-08-07 19:04:41 +0700

    search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,5 +10,16 @@ class PagesController < ApplicationController
   def search
     @categories = Category.all
     @category = Category.find(params[:category]) if params[:category].present?
+    # @gigs = Gig.where("category_id = ? AND gigs.title ILIKE ?", params[:category], "%#{params[:q]}%")
+
+    if params[:category].present? && params[:q].present?
+      @gigs = Gig.where("category_id = ? AND gigs.title ILIKE ?", params[:category], "%#{params[:q]}%")
+    elsif params[:category].present?
+      @gigs = Gig.where("category_id = ?", params[:category])
+    elsif params[:category].blank? && params[:q].present?
+      @gigs = Gig.where("gigs.title ILIKE ?", "%#{params[:q]}%")
+    else
+      @gigs = Gig.all
+    end
   end
 end

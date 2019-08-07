1b6071d0fd0381f0ef5a6802bf498ce07ad8eccd
thule <lunthu@gmail.com>
2019-08-07 16:25:03 +0700

    search menu

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
+
+  def index
+    @categories = Category.all
+
+  end
   def home
+  end
+
+  def search
+    @categories = Category.all
+    @category = Category.find(params[:category]) if params[:category].present?
   end
 end

c8132212a716f0a5197c2b411ae2e0fa3fa3e766
thule <lunthu@gmail.com>
2019-08-13 17:16:30 +0700

    comment with attache-file but still not work as request's attache-file

---
+++ app/channels/comment_channel.rb
@@ -0,0 +1,8 @@
+class CommentChannel < ApplicationCable::Channel
+  def subscribed
+    order = Order.find params[:order]
+    stream_for order
+  end
+
+
+end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,29 +6,43 @@ class CommentsController < ApplicationController
     order = Order.find(comment_params[:order_id])
 
     if comment_params[:content].blank?
-      return redirect_to request.referrer, alert: "Invalid message"
+      #return redirect_to request.referrer, alert: "Invalid message"
+      return render json: {success: false}
+
+    end
+
+    if order.buyer_id != current_user.id && order.seller_id != current_user.id
+      return render json: {success: false}
 
     end
 
     @comment = Comment.new(
                 user_id: current_user.id,
                 order_id: order.id,
-                content: comment_params[:content])
+                content: comment_params[:content],
+                attachment_file: comment_params[:attachment_file])
 
     if @comment.save
-      return redirect_to request.referrer, notice: "Comment sent ..."
+      #return redirect_to request.referrer, notice: "Comment sent ..."
+      CommentChannel.broadcast_to order, message: render_comment(@comment)
+      return render json:{success: true}
 
     else
-      return redirect_to request.referrer, alert: "Cannot create comment"
-
+      #return redirect_to request.referrer, alert: "Cannot create comment"
+      return render json: {success: false}
     end
 
   end
 
   private
 
+  def render_comment(comment)
+    self.render_to_string partial: 'orders/comment', locals: {comment: comment}
+
+  end
+
   def comment_params
-    params.require(:comment).permit(:content, :order_id)
+    params.require(:comment).permit(:content, :order_id, :attachment_file)
 
   end
 
@@ -37,3 +51,14 @@ class CommentsController < ApplicationController
 
   end
 end
+
+
+
+
+
+
+
+
+
+
+

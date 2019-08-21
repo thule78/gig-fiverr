600aa966cec58ebb3aed1c22df6ba151b81b1b40
thule <lunthu@gmail.com>
2019-08-21 18:06:40 +0700

    fix registrations new

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users,
             path: '',
             path_names: {sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout'},
-            controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registration' }
+            controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   root to: 'pages#home'
 
   get '/dashboard', to: 'users#dashboard'

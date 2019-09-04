cdcc457cce07373ab3f6ee78dccbcb7a766c831f
thule <lunthu@gmail.com>
2019-09-04 14:27:59 +0700

    gigs_controller fix error gig_new

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,28 +1,29 @@
 class GigsController < ApplicationController
+
   protect_from_forgery except: [:upload_photo]
   before_action :authenticate_user!, except: [:show]
   before_action :set_gig, except: [:new, :create]
   before_action :is_authorised, only: [:edit, :update, :upload_photo, :delete_photo]
   before_action :set_step, only: [:update, :edit]
-  before_action :set_category, only: [:show, :edit, :new]
+
   def new
     @gig = current_user.gigs.build
-    set_category
+    @categories = Category.all
   end
 
   def create
     @gig = current_user.gigs.build(gig_params)
 
     if @gig.save
-      @gig.pricings.create(Pricing.pricing_types.values.map{|x| {pricing_type: x}})
-      return redirect_to edit_gig_path(@gig), notice: "Save..."
+      @gig.pricings.create(Pricing.pricing_types.values.map{ |x| {pricing_type: x} })
+      redirect_to edit_gig_path(@gig), notice: "Save..."
     else
-      return redirect_to request.referrer, flash: {error: @gig.errors.full_messages}
+      redirect_to request.referrer, flash: { error: @gig.errors.full_messages }
     end
   end
 
   def edit
-    set_category
+    @categories = Category.all
   end
 
   def update
@@ -33,19 +34,18 @@ class GigsController < ApplicationController
           next;
         else
           if pricing[:title].blank? || pricing[:description].blank? || pricing[:delivery_time].blank? || pricing[:price].blank?
-            return redirect_to request.referrer, flash:{error: "Invalid pricing"}
+            return redirect_to request.referrer, flash: {error: "Invalid pricing"}
           end
         end
       end
     end
 
-
     if @step == 3 && gig_params[:description].blank?
-      return redirect_to request.referrer, flash:{error: "Description cannot be blank"}
+      return redirect_to request.referrer, flash: {error: "Description cannot be blank"}
     end
 
     if @step == 4 && @gig.photos.blank?
-      return redirect_to request.referrer, flash:{error: "You don't have any photos"}
+      return redirect_to request.referrer, flash: {error: "You don't have any photos"}
     end
 
     if @step == 5
@@ -54,15 +54,15 @@ class GigsController < ApplicationController
           next;
         else
           if pricing[:title].blank? || pricing[:description].blank? || pricing[:delivery_time].blank? || pricing[:price].blank?
-            return redirect_to edit_gig_path(@gig, step: 2), flash:{error: "Invalid Pricing"}
+            return redirect_to edit_gig_path(@gig, step: 2), flash: {error: "Invalid pricing"}
           end
         end
       end
 
       if @gig.description.blank?
-        return redirect_to edit_gig_path(@gig, step: 3), flash:{error: "Description cannot be blank"}
+        return redirect_to edit_gig_path(@gig, step: 3), flash: {error: "Description cannot be blank"}
       elsif @gig.photos.blank?
-        return redirect_to edit_gig_path(@gig, step: 4), flash:{error: "You don't have any photos"}
+        return redirect_to edit_gig_path(@gig, step: 4), flash: {error: "You don't have any photos"}
       end
     end
 
@@ -73,36 +73,36 @@ class GigsController < ApplicationController
     end
 
     if @step < 5
-      return redirect_to edit_gig_path(@gig, step: @step + 1)
+      redirect_to edit_gig_path(@gig, step: @step + 1)
     else
-      return redirect_to dashboard_path
+      redirect_to dashboard_path
     end
+
   end
 
   def show
-    set_category
+    @categories = Category.all
   end
 
   def upload_photo
     @gig.photos.attach(params[:file])
-    render json: { success: true}
+    render json: { success: true }
   end
 
   def delete_photo
     @image = ActiveStorage::Attachment.find(params[:photo_id])
     @image.purge
-    return redirect_to edit_gig_path(@gig, step: 4)
+    redirect_to edit_gig_path(@gig, step: 4)
   end
 
   def checkout
 
     subscription = Subscription.find_by_user_id(current_user.id)
     if subscription.present? && subscription.success?
-      plan = Stripe::plan.retrieve(subscription.plan_id)
-      @rate = plan.metadata.commission.to_f/100
+      plan = Stripe::Plan.retrieve(subscription.plan_id)
+      @rate =  plan.metadata.commission.to_f/100
     else
       @rate = 10.0/100
-
     end
 
     if current_user.stripe_id
@@ -111,20 +111,14 @@ class GigsController < ApplicationController
       @gig = Gig.find(params[:id])
       @pricing = @gig.pricings.find_by(pricing_type: params[:pricing_type])
     else
-      redirect_to setting_payment_path, alert: "Please add yor card first"
+      redirect_to settings_payment_path, alert: "Please add your card first"
     end
-
   end
 
   private
 
-  def set_category
-    @categories = Category.all
-  end
-
   def set_step
     @step = params[:step].to_i > 0 ? params[:step].to_i : 1
-
     if @step > 5
       @step = 5
     end
@@ -132,7 +126,6 @@ class GigsController < ApplicationController
 
   def set_gig
     @gig = Gig.find(params[:id])
-
   end
 
   def is_authorised
@@ -140,8 +133,7 @@ class GigsController < ApplicationController
   end
 
   def gig_params
-    params.require(:gig).permit(:title, :video, :active, :category_id, :has_single_pricing, :description, :photos[],
+    params.require(:gig).permit(:title, :video, :description, :active, :category_id, :has_single_pricing,
                                 pricings_attributes: [:id, :title, :description, :delivery_time, :price, :pricing_type])
-
   end
 end
